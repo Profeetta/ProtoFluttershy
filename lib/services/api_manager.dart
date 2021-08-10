a5c9cbf15bda73d857a69a4796e3a4fd5eac8cc4
Profeetta <71485903+Profeetta@users.noreply.github.com>
2021-08-10 14:57:41 +0300

    22
    22222

--- lib/services/api_manager.dart
+++ lib/services/api_manager.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 import 'package:http_test_run/constants/strings.dart';
@@ -12,6 +13,9 @@ class API_Manager {
     if (response.statusCode == 200) {
       var jsonString = response
           .body; //This is where we get the jSon. Now we need to convert the json into a model.
+      var jsonMap = json.decode(jsonString);
+
+      //var newsModel = NewsModel.fromJson(jsonMap);  >> newsmodel on jsonin class
     }
   }
 }
